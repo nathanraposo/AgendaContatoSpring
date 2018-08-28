bf9f1d0dd3bdb6b219363e405d94573a3edd9c88
Nathan Raposo <nathanraposoolivo@gmail.com>
2018-08-28 16:31:01 -0300

    Alterações contato endpoint

--- src/main/java/br/com/agendacontatospring/repository/ContatoRepository.java
+++ src/main/java/br/com/agendacontatospring/repository/ContatoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ContatoRepository extends CrudRepository<Contato, Long> {
-    List<Contato> findByName(String name);
+    List<Contato> findByNameIgnoreCaseContaining(String name);
 }
